725922854fae9d6f9fddf9ca3c4353abe9176da2
DESKTOP-HB65II7\Justin <jroy@sociusmarketing.com>
2020-05-20 22:39:52 -0400

    Connect to MongoDB Connect get Transactions Connect addTransaction

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,50 @@
+const Transaction = require('../models/Transaction');
+
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET Transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
+        
+        return res.status(200).json({
+            sucess: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.status(500).json({
+           success: false,
+           error: 'Server Error' 
+        });
+    }
 }
 
 // @desc    Add transaction
 // @route   POST /api/v1/transactions
 // @access Public
-exports.addTransaction = (req, res, next) => {
-    res.send('POST Transactions');
+exports.addTransaction = async (req, res, next) => {
+    try {
+        const { text, amount } = req.body;
+        
+        const transaction = await Transaction.create(req.body);
+        
+        return res.status(201).json({
+           success: true,
+           data: transaction 
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 // @desc    Delete transaction
 // @route   DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
-    res.send('DELETE Transactions');
+exports.deleteTransaction = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        
+    }
 }
