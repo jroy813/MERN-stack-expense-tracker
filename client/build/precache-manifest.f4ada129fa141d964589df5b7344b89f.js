e6a4e563542188a34252025995b05e9b5fcc4a19
DESKTOP-HB65II7\Justin <jroy@sociusmarketing.com>
2020-05-25 21:48:00 -0400

    Run Build

---
+++ client/build/precache-manifest.f4ada129fa141d964589df5b7344b89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bd8717d7b7b85640353dc84899cefd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40b64d6796f46dc4e61",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "902c120a421a2ff062f6",
+    "url": "/static/js/2.9fa54104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fa54104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40b64d6796f46dc4e61",
+    "url": "/static/js/main.975a1b9c.chunk.js"
+  },
+  {
+    "revision": "a535561e400729fe11ba",
+    "url": "/static/js/runtime-main.28a3c6df.js"
+  }
+]);
